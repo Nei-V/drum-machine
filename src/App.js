93258b4bc5ed4eb3f723d612d99f19aa5bfb2281
Ion <19718355+Nei-V@users.noreply.github.com>
2018-08-05 21:37:16 +0300

    started working on redux store

--- src/App.js
+++ src/App.js
@@ -1,70 +1,134 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {connect} from 'react-redux';
 import './App.css';
-import mp3_file from './town.mp3';
+import {SET1,SET2, GOODKEYS,FOOTER} from './constants';
+//set1: RIDE, RIVER, PIERREBASIC, PIERRE, MOODY, NEWWAVE, JUMP, MAXO, FUNKY,
+//set2: FAN, DRO, AAP, ZAYLEAD, TOWN, STABS, CHANGE, BELL, BOUNCE
 
-class App extends Component{
-    constructor (props) {
-      super (props);
-     // this.pressButton=this.pressButton.bind(this);
-     this.playAudio=this.playAudio.bind(this);
-     this.pauseAudio=this.pauseAudio.bind(this);
+
+
+class Footer extends Component {
+  render() {
+    function createFooter(a) {
+      return {__html:a}
     }
+    return (
+      <div id="footerInReact" dangerouslySetInnerHTML={createFooter(FOOTER)}>
+      </div>
+    )
+  }
+}
 
-    playAudio() { 
-      let x = document.getElementById("myAudio"); 
-      x.play(); 
-      console.log("play, x is: ",x);
-  } 
-  
-  pauseAudio() { 
-    let x = document.getElementById("myAudio"); 
-      x.pause(); 
-      console.log("pause");
-  } 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.playAudio = this.playAudio.bind(this);
+    //this.pauseAudio = this.pauseAudio.bind(this);
+    this.playAudioOnKey = this.playAudioOnKey.bind(this);
    
+  }
+componentDidMount(){
+  document.addEventListener('keydown',this.playAudioOnKey);
   
+}
+componentWillUnmount(){
+  document.removeEventListener('keydown',this.playAudioOnKey);
+}
 
-    /*
-    pressButton(e){
-      this.props.keyPressed(event.target.value);
-      console.log(event.target.value);
+
+  playAudio(e) {
+    let x=e.target.firstElementChild;
+    x.play();
+    console.log("play, x is: ", x);
     }
+
+ /*
+  pauseAudio() {
+    //let x=e.target.firstElementChild;
+    x.pause();
+    console.log("pause",x);
+  }
 */
 
-
-
-    render(){
-
-      
-     
-  
-     
-      return(
-        <div>
-          <p>Click the buttons to play or pause the audio.</p>
-          <button onClick={this.playAudio} type="button">Play Audio</button>
-  <button onClick={this.pauseAudio} type="button">Pause Audio</button> 
-<audio id="myAudio">
-  <source src="https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156862/sounds/town.mp3" type="audio/mpeg" />
-</audio>
-
-          </div>
-      )
+  playAudioOnKey(e) {
+    e.preventDefault();
+     console.log("key pressed (keycode)",e);
+    let z=e.key;
+    console.log("z: ",z)
     
-  }
+    if (GOODKEYS.includes(z)){
+      let uppecaselikeID=document.getElementById(z.toUpperCase());
+      uppecaselikeID.play();
+      console.log(uppecaselikeID);
+    }     
+    }
+  
+
 
   /*
+  pressButton(e){
+    this.props.keyPressed(event.target.value);
+    console.log(event.target.value);
+  }
+*/
 
-  
-  
-  
-  
-  <script>
-  
-  </script>
+  render() {
+    
+    return (
+      <div>
+        <p>Click the buttons to play or pause the audio.</p>
+        
+        <button id="ride" onClick={this.playAudio} onKeyPress={this.playAudioOnKey} className="drum-pad">Q
+        <audio id="Q" className="clip">
+          <source src={SET1.RIDE} type="audio/mpeg" />
+        </audio>
+        </button>
+      
+        <button id="river" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">W
+        <audio id="W" className="clip">
+          <source src={SET1.RIVER} type="audio/mpeg" />
+        </audio>
+        </button>
+        <button id="pierrebasic" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">E
+        <audio id="E" className="clip">
+          <source src={SET1.PIERREBASIC} type="audio/mpeg" />
+        </audio></button>
+        <button id="pierre" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">A
+        <audio id="A" className="clip">
+          <source src={SET1.PIERRE} type="audio/mpeg" />
+        </audio></button>
+        <button id="moody" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">S
+        <audio id="S" className="clip">
+          <source src={SET1.MOODY} type="audio/mpeg" />
+        </audio></button>
+        <button id="newwave" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">D
+        <audio id="D" className="clip">
+          <source src={SET1.NEWWAVE} type="audio/mpeg" />
+        </audio></button>
+        <button id="jump" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">Z
+        <audio id="Z" className="clip">
+          <source src={SET1.JUMP} type="audio/mpeg" />
+        </audio></button>
+        <button id="maxo" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">X
+        <audio id="X" className="clip">
+          <source src={SET1.MAXO} type="audio/mpeg" />
+        </audio></button>
+        <button id="funky" onClick={this.playAudio} onKeyDown={this.playAudioOnKey} type="button" className="drum-pad">C
+        <audio id="C" className="clip">
+          <source src={SET1.FUNKY} type="audio/mpeg" />
+        </audio></button>
+      <dialog id="display" />Played Sound
+       <Footer />
+        
 
-  */
+      </div>
+    )
+
+  }
+
 }
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,54 @@
+
+export const SET1 = {
+    RIDE: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156865/sounds/Ride.mp3",
+    RIVER: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156865/sounds/River.mp3",
+    PIERREBASIC: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/PierreBasic.mp3",
+    PIERRE: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/Pierre.mp3",
+    MOODY: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/Moody.mp3",
+    NEWWAVE: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/NewWave.mp3",
+    JUMP: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/Jump.mp3",
+    MAXO: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156864/sounds/Maxo.mp3",
+    FUNKY: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156863/sounds/Funky.mp3"
+}
+
+export const SET2 = {
+    FAN: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156863/sounds/Fan.mp3",
+    DRO: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156863/sounds/Dro.mp3",
+    AAP: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156863/sounds/AAP.mp3",
+    ZAYLEAD: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156863/sounds/ZayLead.mp3",
+    TOWN: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156862/sounds/town.mp3",
+    STABS: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156862/sounds/Stabs.mp3",
+    CHANGE: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156862/sounds/change.mp3",
+    BELL: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156862/sounds/Bell.mp3",
+    BOUNCE: "https://res.cloudinary.com/dg45lvfuu/video/upload/v1533156861/sounds/Bounce.mp3"
+}
+
+export const GOODKEYS=["q","w","e","a","s","d","z","x","c"];
+
+
+export const FOOTER=`
+<footer>
+<p>Made by
+  <a href="https://codepen.io/Nei_V" target="_blank">Ion Varsescu</a>
+</p>
+<section id="contact">
+  <a href="https://www.facebook.com/varsescu.ion" target="_blank">
+    <i class="fa fa-facebook-square fa-3x"></i>
+  </a>
+  <a href="https://twitter.com/IonVcoding" target="_blank">
+    <i class="fa fa-twitter fa-3x"></i>
+  </a>
+  <a href="https://www.linkedin.com/in/ionvarsescu/" target="_blank">
+    <i class="fa fa-linkedin fa-3x"></i>
+  </a>
+  <a href="https://plus.google.com/100730160336168112160" target="_blank">
+    <i class="fa fa-google-plus fa-3x"></i>
+  </a>
+  <a href="https://github.com/Nei-V" target="_blank">
+    <i class="fa fa-github fa-3x"></i>
+  </a>
+  <a href="https://www.freecodecamp.org/portofolio/nei-v" target="_blank">
+    <img src="https://res.cloudinary.com/dg45lvfuu/image/upload/v1465845327/fcc-app-icon_gqbnku.png" />
+  </a>
+</section>
+</footer>`
